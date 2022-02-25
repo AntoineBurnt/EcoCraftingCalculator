462fedf525104b125f9fd2a792dff4828fb97786
AntoineBurnt <antoinebrule@cmail.carleton.ca>
2022-02-25 11:46:26 -0500

    Added comments and doc strings

--- ECC.py
+++ ECC.py
@@ -20,11 +20,41 @@ class Component(object):
         self.table = table
         self.isEnd = isEnd
 
-    def getRecipe(self):
+    def getRecipe(self) -> dict:
+        """
+        Returns the cost of itself as a dictionary.
+        
+        >>>loadedComps["steam truck"].getRecipe()
+        {'iron plate': 12, 'iron pipe': 8, 'screws': 24, 'portable steam engine': 1, 'iron wheel': 4, 'iron axle': 1}
+        """
         return self.cost
+    
     #Dont touch first it's to differentiate betwwen the recursive calls and the user asking for the costs
     #Additive stops it from clearing the "rawNeeded" list after printing the results
-    def rawCosts(self,n,first = True,additive = False): 
+    def rawCosts(self,n: int,additive = False,first = True) -> None:
+        """
+        Calculates the costs of the "n" amnount of the component in terms of the
+        lowest crafting component, raw resources. Prints the results to the console
+        when finished. The additive argument can be set to true to add the consts
+        if multiple recipes together, rawNeeded.clear() is used to clear the results.
+        
+        >>>loadedComps["steam truck"].rawCosts(1)
+        Calcs Complete
+        {'iron bars': 182}
+        >>>loadedComps["portable steam engine"].rawCosts(10)
+        Calcs Complete
+        {'iron bars': 1290}
+        
+        >>loadedComps["steam truck"].rawCosts(1,True)
+        Calcs Complete
+        {'iron bars': 182}
+        >>>loadedComps["steam truck"].rawCosts(1,True)
+        Calcs Complete
+        {'iron bars': 364}
+        >>>loadedComps["portable steam engine"].rawCosts(10)
+        Calcs Complete
+        {'iron bars': 1654}
+        """
         
         if self.isEnd:
             rawNeeded[self.name] = n + rawNeeded.get(self.name,0)
@@ -38,8 +68,16 @@ class Component(object):
             print(rawNeeded)
             if not additive:
                 rawNeeded.clear() #Print in console to manually clear results
-    #["iron gear","iron plate","screws","iron pipe","iron wheel","iron axle","fiberglass","steel plate","steel gear","steel gearbox","steel axle"]
-    def targetedCosts(self,n,stop,first = True,additive = False):
+    
+    def targetedCosts(self,n: int,stop: list,additive = False,first = True) -> None:
+        """
+        Works similar to rawCosts but stop is used to defined the end components
+        instead of using the lowest one.
+        
+        >>>loadedComps["skidsteer"].targetedCosts(1,["iron gear","iron plate","screws","iron pipe","iron wheel","iron axle","fiberglass","steel plate","steel gear","steel gearbox","steel axle"])
+        Calcs Complete
+        {'iron bars': 20, 'iron gear': 24, 'cellulose fibers': 8, 'steel plate': 56, 'steel bars': 68, 'iron pipe': 12, 'steel gearbox': 6, 'fiberglass': 174, 'epoxy': 144, 'gold bars': 138, 'copper bars': 432, 'synthetic rubber': 32, 'steel axle': 1}
+        """
         condition = False
         for x in stop:
             if x == self.name:
@@ -63,6 +101,7 @@ class Component(object):
 
 
 def loadComps():
+    #Load all the components from the text file, shouldn't ever need to call this function manualy 
     loadedComps.clear()
     with open("Components.txt","r") as file:
 
